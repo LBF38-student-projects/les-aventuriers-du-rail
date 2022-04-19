c8f8f9526baea0fdbca9d531796614b33b183e4f
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-04-19 10:52:43 +0200

    Ajout de variables pour définir les caractéristiques du joueur pendant la partie.

--- src/Joueur.py
+++ src/Joueur.py
@@ -3,11 +3,40 @@
 
 
 class Joueur():
-    """Classe qui définit les attributs et cartes du joueur d'une partie.
-    Il y aura ici les joueurs IA et humains. A nous de les définir comme on le souhaite."""
-    def __init__(self):
-        self.joueur='name'
-        #A compléter
+    """
+    Classe qui définit les attributs et cartes du joueur d'une partie.
+    Il y aura ici les joueurs IA et humains. A nous de les définir comme on le souhaite.
+    """
+    def __init__(self,nom_couleur,points):
+        self.joueur='name'          # Nom du joueur
+        self.wagons=45              # Nombre de wagons maximum par joueur
+        self.couleur=nom_couleur    # Définit la couleur du joueur
+        self.nb_points=points       # Définit le marqueur de points du joueur
+        self.mains={}               # Définit la main actuelle du joueur => contient toutes ses cartes
+
+    @property
+    def couleur(self):
+        """
+        Accesseur de la variable couleur
+        :return: valeur de couleur
+        """
+        return self.__couleur
+
+    @couleur.setter
+    def couleur(self,nom_couleur):
+        """
+        Définit la valeur du paramètre et vérifie les conditions de définition
+        :param nom_couleur: str
+        :return: None
+        """
+        assert type(nom_couleur)==str
+        self.__couleur=nom_couleur
+
+
+
+
+
+
 
 
 
