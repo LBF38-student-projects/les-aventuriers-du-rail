6c96a5a9666da9e90f4170f84115683dcc308d85
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-04-21 11:45:28 +0200

    Règles et structure mise en place. Code à implémenter

--- src/Partie.py
+++ src/Partie.py
@@ -14,8 +14,6 @@ class Partie():
     def __init__(self):
         self.les_joueurs = []
         self.nb_joueurs: int = 0
-        self.carte = 'carte'
-        # A compléter
 
     def debut_partie(self):
         """
@@ -34,7 +32,11 @@ class Partie():
         L'ordre des joueurs de la partie
         """
         joueur_voyageur = input("Qui a le plus voyagé entre ces joueurs ? \n" + self.liste_joueurs())
-        index_premier = self.les_joueurs.index(joueur_voyageur)
+        index_premier = 0
+        j = self.les_joueurs[0]
+        while j.nom_joueur != joueur_voyageur:
+            index_premier += 1
+            j = self.les_joueurs[index_premier]
         if index_premier == 0:
             return "L'ordre des joueurs est établi comme suit : \n" + self.liste_joueurs()
         else:
@@ -56,11 +58,85 @@ class Partie():
             liste_joueurs += joueur.nom_joueur + "\n"
         return liste_joueurs
 
-    def tour(self):
+    def tour(self, joueur):
         """
         Réalise 1 tour pour 1 joueur de toutes les actions possibles.
         :return:
         """
+        print("C'est le tour de " + joueur.nom_joueur)
+        choix: int = int(input(
+            "Que voulez-vous faire ?\n" + "1. Prendre des cartes Wagon\n2. Prendre possession d'une route\n3. "
+                                          "Prendre des cartes Destination supplémentaires\nIndiquer le numéro "
+                                          "de l'action choisie"))
+        if choix == 1:
+            self.prendre_cartes_wagon()
+        elif choix == 2:
+            self.prendre_route()
+        else:
+            self.prendre_cartes_Destination()
+
+    def prendre_cartes_wagon(self):
+        """
+         1. Prendre des cartes Wagon :
+        – le joueur peut prendre 2 cartes Wagon.
+        Il peut prendre n’importe quelle carte visible parmi les 5 posées sur la table ou tirer une carte du dessus de
+        la pioche (tirage en aveugle). Si le joueur prend une carte visible, il la remplace immédiatement par une
+        autre du dessus de la pioche. Il peut ensuite prendre une deuxième carte, soit visible, soit en aveugle (voir
+        section Cartes Wagon pour les cartes Locomotive).
+        :return:
+        """
+        """Indication sur les cartes Wagon : 
+        Cartes Wagon 
+        
+        Il existe 8 types de wagons différents en plus de la locomotive. Les couleurs de chaque carte Wagon 
+        correspondent aux couleurs présentes sur le plateau afin de relier les villes : bleu, violet, orange, blanc, 
+        vert, jaune, noir et rouge. Les locomotives sont multicolores et, comme des cartes joker, peuvent remplacer 
+        n’importe quelle couleur lors de la prise de possession d’une route. 
+        
+        Il est possible de ne jouer que des cartes Locomotive pour prendre une route. Si une carte Locomotive figure 
+        parmi les 5 cartes visibles, le joueur peut la prendre, mais son tour s’arrête alors. La locomotive compte 
+        comme si l’on avait pris 2 cartes. Si, après avoir pris une carte visible (qui n’est pas une locomotive), 
+        la carte de remplacement est une locomotive, le joueur ne peut pas la prendre. Si, au cours du jeu, 
+        3 cartes visibles sur 5 sont des locomotives, les 5 cartes sont alors immédiatement défaussées et remplacées 
+        par 5 nouvelles cartes. 
+        
+        Note : Si un joueur a la chance de tirer une locomotive en aveugle, au sommet de la pioche, les autres 
+        joueurs n’en savent rien et cette carte ne compte que pour une seule carte piochée. Un joueur peut avoir en 
+        main et à tout moment autant de cartes qu’il le souhaite. Quand la pioche de cartes est épuisée, les cartes 
+        défaussées sont soigneusement mélangées pour reconstituer une nouvelle pioche. Il est important de bien 
+        mélanger les cartes car elles ont été défaussées par séries de couleurs ! Dans le cas peu probable où il n’y 
+        aurait plus de cartes disponibles (si toutes les cartes sont dans les mains des joueurs) un joueur ne peut 
+        plus prendre de cartes. Il ne peut donc que prendre possession d’une route ou tirer de nouvelles cartes 
+        Destination. """
+
+        print("Je prends des cartes Wagon")
+        print("A implémenter")
+        return None
+
+    def prendre_route(self):
+        """
+        2. Prendre possession d’une route :
+        – Le joueur peut s’emparer d’une route sur le plateau en posant autant de cartes Wagon de la couleur
+        de la route que d’espaces composant la route. Après avoir défaussé ses cartes, le joueur pose alors ses wagons
+        sur chaque espace constituant la route. Enfin, il déplace son marqueur de score en se référant au tableau de
+        décompte des points.
+        :return:
+        """
+        print("Je prends une route")
+        print("A implémenter")
+        return None
+
+    def prendre_cartes_Destination(self):
+        """
+        3. Prendre des cartes Destination supplémentaires :
+        – Le joueur prend 3 cartes Destination du dessus de la pioche. Il doit en conserver au moins une,
+        mais peut aussi garder 2 ou 3 cartes. Chaque carte qui n’est pas conservée est posée face cachée sous la pioche
+        des cartes Destination.
+        :return:
+        """
+        print("Je prends une carte Destination")
+        print("A implémenter")
+        return None
 
     def partie(self):
         """
@@ -68,12 +144,34 @@ class Partie():
         :return:
         """
         self.debut_partie()
+
         """
-        Règle du jeu :
-        Tour de jeu
-        Le joueur qui a le plus voyagé commence. Par la suite, on joue dans le sens des aiguilles d’une montre. 
-        À son tour, le joueur doit faire une et une seule des trois actions suivantes :"""
+        # Règle du jeu :
+        ## Tour de jeu
+        Le joueur qui a le plus voyagé commence."""
         self.choix_tour_joueur()
+        """Par la suite, on joue dans le sens des aiguilles d’une montre. 
+        À son tour, le joueur doit faire une et une seule des trois actions suivantes : 
+        1. Prendre des cartes Wagon : 
+        – le joueur peut prendre 2 cartes Wagon. 
+        Il peut prendre n’importe quelle carte visible parmi les 5 posées sur la table ou tirer une carte du dessus de 
+        la pioche (tirage en aveugle). Si le joueur prend une carte visible, il la remplace immédiatement par une 
+        autre du dessus de la pioche. Il peut ensuite prendre une deuxième carte, soit visible, soit en aveugle (voir 
+        section Cartes Wagon pour les cartes Locomotive). 
+        
+        2. Prendre possession d’une route : 
+        – Le joueur peut s’emparer d’une route sur le plateau en posant autant de cartes Wagon de la couleur 
+        de la route que d’espaces composant la route. Après avoir défaussé ses cartes, le joueur pose alors ses wagons 
+        sur chaque espace constituant la route. Enfin, il déplace son marqueur de score en se référant au tableau de 
+        décompte des points.
+        
+        3. Prendre des cartes Destination supplémentaires :
+        – Le joueur prend 3 cartes Destination du dessus de la pioche. Il doit en conserver au moins une, 
+        mais peut aussi garder 2 ou 3 cartes. Chaque carte qui n’est pas conservée est posée face cachée sous la pioche 
+        des cartes Destination.
+        """
+        for joueur in self.les_joueurs:
+            print(self.tour(joueur))  # Tour à implémenter avec chaque action possible.
 
 
 if __name__ == '__main__':
