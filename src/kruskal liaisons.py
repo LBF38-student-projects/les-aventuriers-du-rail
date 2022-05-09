bd0ac13e0b7ad2334148fa22f4c1338b12cb80c3
belaidke <kenza.belaid@ensta-bretagne.org>
2022-05-09 10:48:31 +0200

    code Kruskal : arbre couvrant minimal

--- src/kruskal liaisons.py
+++ src/kruskal liaisons.py
@@ -1,7 +1,7 @@
 """Projet Informatique
 @authors: Mathis URIEN, Kenza BELAID"""
 
-from Joueur import Joueur as jr
+#from Joueur import Joueur as jr
 #from Partie import Partie as pt
 
 class EnsembleDisjoint:
@@ -35,6 +35,7 @@ def Kruskal(liaisons, nombre_villes):
     arbre_minimal =[]
     ed = EnsembleDisjoint(nombre_villes)
     index = 0
+
     while len(arbre_minimal) != nombre_villes - 1 :
         (depart, arrivee, longueur) = liaisons[index]
         index = index + 1
@@ -46,47 +47,6 @@ def Kruskal(liaisons, nombre_villes):
             arbre_minimal.apppend((depart, arrivee, longueur))
             ed.Union(x,y)
         return arbre_minimal
-
-class Score():
-    """Classe qui implémente les différentes méthodes pour calculer les joueur.nb_points du jeu
-    => Il pourra y avoir plusieurs méthodes de calcul des joueur.nb_points sur le graphe. A voir selon le temps qu'on a."""
-    def __init__(self,partie):
-        self.compte='compte le score'
-        self.partie=partie
-        #self.tableau_score = {
-            #1 : 1,
-            #2
-        #}
-        #A compléter
-        #nb_joueurs = pt.nb_joueurs
-        #L = nb_joueurs*[0]
-        #for i in range(nb_joueurs):
-            #L[i] = []
-        Objectifs = jr.main_cartes['Destination']
-
-    def calcul_pts_destinations(self,joueur):
-        joueur.nb_points = 0
-        for r in self.joueur.route_prise :
-            nv_nom_route = "{0} to {1}".format(r[0], r[1])
-            if nv_nom_route not in self.joueur.mains_cartes['Destination']:
-                nv_nom_route = "{0} to {1}".format(r[1], r[0])
-            elif nv_nom_route not in self.joueur.mains_cartes['Destination']:
-                joueur.nb_points = joueur.nb_points
-            else :
-                joueur.nb_points += self.partie.carte_destination["nv_nom_route"]
-
-    #def calcul_pts_route(self,joueur):
-        #for r in self.joueur.route_prise :
-            #ville1 = r[0]
-            #ville2 = r[1]
-            #joueur.nb_points += self.partie.liens_villes[ville1][ville2][1]
-
-    #def calcul_plus_long_chemin(self,joueur):
-        #for r in self.joueur.route_prise :
-            #ville =
-
-
-
 
 
 if __name__ == '__main__':
