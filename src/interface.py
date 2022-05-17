5bf5764e85aa040b839e752cb5c61c2731b0af0b
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-05-17 13:21:01 +0200

    Work session
    interface.py:
    - interface de base, carte
    - plusieurs tests pour les boutons et les text input
    => A continuer
    
    kruskal liaisons.py:
    - Reformatage du fichier

--- src/interface.py
+++ src/interface.py
@@ -9,11 +9,112 @@ from pygame.locals import *
 from Partie import *
 
 
+class Screen():
+    def __init__(self, title, width=640, height=445, fill="white"):
+        self.title = title
+        self.width = width
+        self.height = height
+        self.fill = fill
+        self.current = False
+
+    def make_current(self):
+        pygame.display.set_caption(self.title)
+        self.current = True
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+    def end_current(self):
+        self.current = False
+
+    def check_update(self):
+        return self.current
+
+    def screen_update(self):
+        if self.current:
+            self.screen.fill(self.fill)
+
+    def return_title(self):
+        return self.screen
+
+
+class Button():
+    def __init__(self, x, y, sx, sy, bcolour, fbcolour, font, fontsize, fcolour, text):
+        self.x = x
+        self.y = y
+        self.sx = sx
+        self.sy = sy
+        self.bcolour = bcolour
+        self.fbcolour = fbcolour
+        self.fcolour = fcolour
+        self.fontsize = fontsize
+        self.text = text
+        self.current = False
+        self.buttonf = pygame.font.SysFont(font, fontsize)
+
+    def show_button(self, display):
+        if self.current:
+            pygame.draw.rect(display, self.fbcolour, (self.x, self.y, self.sx, self.sy))
+        else:
+            pygame.draw.rect(display, self.bcolour, (self.x, self.y, self.sx, self.sy))
+        textsurface = self.buttonf.render(self.text, False, self.fcolour)
+        display.blit(textsurface, (self.x + self.sx / 2 - (self.fontsize / 2) * (len(self.text) / 2) - 5,
+                                   (self.y + self.sy / 2 - (self.fontsize / 2) - 4)))
+
+    def focus_check(self, mousepos, mouseclick):
+        if self.x <= mousepos[0] <= self.x + self.sx and self.y <= mousepos[1] <= self.y + self.sy:
+            self.current = True
+            return mouseclick[0]
+        else:
+            self.current = False
+            return False
+
+
+def launch_game():
+    pygame.init()
+    pygame.font.init()
+    menuScreen = Screen("Les aventuriers du rail")
+    screen2 = Screen("Screen 2")
+
+    win = menuScreen.make_current()
+    done = False
+    testButton = Button(0, 0, 150, 50, "black", "blue", "arial", 20, "white", "Menu")
+    returnButton = Button(0, 0, 150, 50, "orange", "green", "arial", 20, "black", "Back")
+
+    toggle = False
+
+    while not done:
+        menuScreen.screen_update()
+        screen2.screen_update()
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_click = pygame.mouse.get_pressed()
+        keys = pygame.key.get_pressed()
+
+        # menuScreen Page code
+        if menuScreen.check_update():
+            screen2button = testButton.focus_check(mouse_pos, mouse_click)
+            testButton.show_button(menuScreen.return_title())
+            if screen2button:
+                win = screen2.make_current()
+                menuScreen.end_current()
+
+        # Screen 2 Page code
+        elif screen2.check_update():
+            # Back Button
+            returnm = returnButton.focus_check(mouse_pos, mouse_click)
+            returnButton.show_button(screen2.return_title())
+            if returnm:
+                win = menuScreen.make_current()
+                screen2.end_current()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        pygame.display.update()
+    pygame.quit()
+
 
 def game():
     size = width, height = 1920 // 2, 1080 // 2
     screen = pygame.display.set_mode(size)
-    run=True
+    run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,21 +122,21 @@ def game():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == K_ESCAPE:
-                    run=False
+                    run = False
         screen.fill("blue")
         pygame.display.flip()
         # screen.blit(plateau, ((width - size_plateau[0]) // 2, (height - size_plateau[1]) // 2))
 
 
-if __name__=='__main__':
+def test():
     pygame.init()
     size = width, height = 1920 // 2, 1080 // 2
     screen = pygame.display.set_mode(size)
     plateau = pygame.image.load("img\carte_usa.jpg")
-    print(plateau.get_width(), plateau.get_height())
+    # print(plateau.get_width(), plateau.get_height())
     wagon = pygame.image.load("img\wagon_test.png")
     pygame.display.set_caption("Les aventuriers du rail")
-    size_plateau=plateau.get_size()
+    size_plateau = plateau.get_size()
     # pygame.display.toggle_fullscreen()
     while True:
         for event in pygame.event.get():
@@ -43,35 +144,109 @@ if __name__=='__main__':
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key== K_ESCAPE:
+                if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-                if event.key==K_g:
+                if event.key == K_g:
                     game()
-            if event.type==pygame.MOUSEBUTTONDOWN:
-                mx,my=pygame.mouse.get_pos()
-                print((mx,my))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mx, my = pygame.mouse.get_pos()
+                print((mx, my))
         screen.fill("grey")
-        screen.blit(plateau, ((width-size_plateau[0])//2, (height-size_plateau[1])//2))
+        screen.blit(plateau, ((width - size_plateau[0]) // 2, (height - size_plateau[1]) // 2))
         # pygame.display.flip()
-        screen.blit(wagon,(10,5))
+        screen.blit(wagon, (369, 191))
         pygame.display.flip()
         # rect=pygame.Rect(1,1,1,1)
         # pygame.draw.rect(screen,"blue",rect)
         # pygame.display.flip()
 
 
+def text_input():
+    # pygame.init() will initialize all
+    # imported module
+    pygame.init()
+
+    clock = pygame.time.Clock()
+
+    # it will display on screen
+    screen = pygame.display.set_mode([600, 500])
+
+    # basic font for user typed
+    base_font = pygame.font.Font(None, 32)
+    user_text = ''
+
+    # create rectangle
+    input_rect = pygame.Rect(200, 200, 140, 32)
+
+    # color_active stores color(lightskyblue3) which
+    # gets active when input box is clicked by user
+    color_active = pygame.Color('lightskyblue3')
+
+    # color_passive store color(chartreuse4) which is
+    # color of input box.
+    color_passive = pygame.Color('chartreuse4')
+    color = color_passive
+
+    active = False
+
+    while True:
+        for event in pygame.event.get():
+
+            # if user types QUIT then the screen will close
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_rect.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
+
+            if event.type == pygame.KEYDOWN:
+
+                # Check for backspace
+                if event.key == pygame.K_BACKSPACE:
+
+                    # get text input from 0 to -1 i.e. end.
+                    user_text = user_text[:-1]
+
+                # Unicode standard is used for string
+                # formation
+                else:
+                    user_text += event.unicode
+
+        # it will set background color of screen
+        screen.fill((255, 255, 255))
+
+        if active:
+            color = color_active
+        else:
+            color = color_passive
+
+        # draw rectangle and argument passed which should
+        # be on screen
+        pygame.draw.rect(screen, color, input_rect)
+
+        text_surface = base_font.render(user_text, True, (255, 255, 255))
+
+        # render at position stated in arguments
+        screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+
+        # set width of textfield so that text cannot get
+        # outside of user's text input
+        input_rect.w = max(100, text_surface.get_width() + 10)
+
+        # display.flip() will update only a portion of the
+        # screen to updated, not full area
+        pygame.display.flip()
+
+        # clock.tick(60) means that for every second at most
+        # 60 frames should be passed.
+        clock.tick(60)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    # launch_game()
+    text_input()
--- src/kruskal liaisons.py
+++ src/kruskal liaisons.py
@@ -1,12 +1,13 @@
 """Projet Informatique
 @authors: Mathis URIEN, Kenza BELAID"""
 
-#from Joueur import Joueur as jr
-#from Partie import Partie as pt
+
+# from Joueur import Joueur as jr
+# from Partie import Partie as pt
 
 class EnsembleDisjoint:
     """
-    Cette classe permet de gérer les ensmebles disjoints. Deux éléments sont
+    Cette classe permet de gérer les ensembles disjoints. Deux éléments sont
     considérés dans le même ensemble s'ils ont le même parent.
     """
     parent = {}
@@ -30,71 +31,71 @@ class EnsembleDisjoint:
 
         self.parent[x] = y
 
+
 def Kruskal(liaisons, nombre_villes):
     """Construction de l'arbre couvrant minimal à l'aide de l'algorithme de Kruskal"""
-    arbre_minimal =[]
+    arbre_minimal = []
     ed = EnsembleDisjoint(nombre_villes)
     index = 0
 
-    while len(arbre_minimal) != nombre_villes - 1 :
+    while len(arbre_minimal) != nombre_villes - 1:
         (depart, arrivee, longueur) = liaisons[index]
         index = index + 1
 
         x = ed.get_parent(depart)
         y = ed.get_parent(arrivee)
 
-        if x != y :
+        if x != y:
             arbre_minimal.apppend((depart, arrivee, longueur))
-            ed.Union(x,y)
+            ed.Union(x, y)
         return arbre_minimal
 
 
 if __name__ == '__main__':
+    # liste des arcs au format (départ, arrivée, longueur)
 
-# liste des arcs au format (départ, arrivée, longueur)
+    liaisons = [("Los Angeles", "San Franciso", 3), ("Los Angeles", "Las Vegas", 2),
+                ("Los Angeles", "Phoenix", 3), ("Los Angeles", "El Paso", 6),
+                ("San Francisco", "Portland", 5), ("San Francisco", "Salt Lake City", 5),
+                ("Portland", "Seattle", 1), ("Portland", "Salt Lake City", 6),
+                ("Seattle", "Helena", 6), ("Seattle", "Vancouver", 1),
+                ("Seattle", "Calgary", 4), ("Vancouver", "Calgary", 3),
+                ("Calgary", "Winnipeg", 6), ("Calgary", "Helena", 4),
+                ("Helena", "Winnipeg", 4), ("Helena", "Duluth", 6),
+                ("Helena", "Omaha", 5), ("Helena", "Denver", 4),
+                ("Helena", "Salt Lake City", 3), ("Salt Lake City", "Las Vegas", 3),
+                ("Salt Lake City", "Denver", 3), ("Phoenix", "El Paso", 3),
+                ("Phoenix", "Santa Fe", 3), ("Phoenix", "Denver", 5),
+                ("El Paso", "Santa Fe", 2), ("El Paso", "Houston", 6),
+                ("El Paso", "Dallas", 4), ("El Paso", "Oklahoma City", 5),
+                ("Santa Fe", "Denver", 2), ("Santa Fe", "Oklahoma City", 3),
+                ("Denver", "Omaha", 4), ("Denver", "Kansas City", 4),
+                ("Oklahoma City", "Kansas City", 2), ("Oklahoma City", "Dallas", 2),
+                ("Oklahoma City", "Little Rock", 2), ("Kansas City", "Omaha", 1),
+                ("Kansas City", "Saint Louis", 2), ("Omaha", "Chicago", 4),
+                ("Omaha", "Duluth", 2), ("Duluth", "Winnipeg", 4),
+                ("Duluth", "Sault Ste Marie", 3), ("Duluth", "Toronto", 6),
+                ("Duluth", "Chicago", 3), ("Sault Ste Marie", "Winnipeg", 6),
+                ("Sault Ste Marie", "Duluth", 3), ("Sault Ste Marie", "Toronto", 2),
+                ("Sault Ste Marie", "Montreal", 5), ("Winnipeg", "Calgary", 6),
+                ("Dallas", "Houston", 1), ("Dallas", "Little Rock", 2),
+                ("Houston", "New Orleans", 2), ("New Orleans", "Little Rock", 3),
+                ("New Orleans", "Atlanta", 4), ("New Orleans", "Miami", 6),
+                ("Little Rock", "Saint Louis", 2), ("Little Rock", "Nashville", 3),
+                ("Saint Louis", "Chicago", 2), ("Saint Louis", "Pittsburgh", 5),
+                ("Saint Louis", "Nashville", 2), ("Chicago", "Toronto", 4),
+                ("Chicago", "Pittsburgh", 3), ("Nashville", "Pittsburgh", 4),
+                ("Nashville", "Atlanta", 1), ("Nashville", "Raleigh", 3),
+                ("Atlanta", "Raleigh", 2), ("Atlanta", "Charleston", 2),
+                ("Atlanta", "Miami", 5), ("Miami", "Charleston", 4),
+                ("Charleston", "Raleigh", 2), ("Pittsburgh", "Washington", 2),
+                ("Pittsburgh", "Raleigh", 2), ("Pittsburgh", "Toronto", 2),
+                ("Pittsburgh", "New York", 2), ("Toronto", "Montreal", 3),
+                ("Montreal", "Boston", 2), ("Montreal", "New York", 3),
+                ("Boston", "New York", 2), ("New York", "Boston", 2),
+                ("Boston", "Washington", 2), ("Washington", "Raleigh", 2,)]
 
-    liaisons = [("Los Angeles", "San Franciso", 3),("Los Angeles", "Las Vegas", 2),
-                ("Los Angeles", "Phoenix", 3),("Los Angeles", "El Paso", 6),
-                ("San Francisco", "Portland", 5),("San Francisco", "Salt Lake City", 5),
-                ("Portland", "Seattle", 1),("Portland", "Salt Lake City", 6),
-                ("Seattle", "Helena", 6),("Seattle", "Vancouver", 1),
-                ("Seattle", "Calgary", 4),("Vancouver","Calgary",3),
-                ("Calgary", "Winnipeg", 6),("Calgary", "Helena", 4),
-                ("Helena", "Winnipeg", 4),("Helena", "Duluth", 6),
-                ("Helena", "Omaha", 5),("Helena", "Denver", 4),
-                ("Helena", "Salt Lake City", 3),("Salt Lake City", "Las Vegas", 3),
-                ("Salt Lake City","Denver",3),("Phoenix","El Paso",3),
-                ("Phoenix","Santa Fe",3),("Phoenix", "Denver", 5),
-                ("El Paso","Santa Fe",2),("El Paso","Houston",6),
-                ("El Paso","Dallas",4),("El Paso", "Oklahoma City", 5),
-                ("Santa Fe","Denver",2),("Santa Fe", "Oklahoma City", 3),
-                ("Denver","Omaha",4),("Denver","Kansas City",4),
-                ("Oklahoma City","Kansas City",2),("Oklahoma City","Dallas",2),
-                ("Oklahoma City","Little Rock",2),("Kansas City","Omaha",1),
-                ("Kansas City", "Saint Louis",2),("Omaha","Chicago",4),
-                ("Omaha", "Duluth", 2),("Duluth","Winnipeg",4),
-                ("Duluth", "Sault Ste Marie", 3),("Duluth","Toronto",6),
-                ("Duluth", "Chicago", 3),("Sault Ste Marie","Winnipeg",6),
-                ("Sault Ste Marie","Duluth",3),("Sault Ste Marie","Toronto",2),
-                ("Sault Ste Marie","Montreal",5),("Winnipeg","Calgary",6),
-                ("Dallas","Houston",1),("Dallas","Little Rock",2),
-                ("Houston","New Orleans",2),("New Orleans","Little Rock",3),
-                ("New Orleans","Atlanta",4),("New Orleans","Miami",6),
-                ("Little Rock","Saint Louis",2),("Little Rock","Nashville",3),
-                ("Saint Louis","Chicago",2),("Saint Louis","Pittsburgh",5),
-                ("Saint Louis","Nashville",2),("Chicago","Toronto",4),
-                ("Chicago","Pittsburgh",3),("Nashville","Pittsburgh",4),
-                ("Nashville","Atlanta",1),("Nashville","Raleigh", 3),
-                ("Atlanta","Raleigh",2),("Atlanta","Charleston",2),
-                ("Atlanta","Miami",5),("Miami","Charleston",4),
-                ("Charleston","Raleigh",2),("Pittsburgh","Washington",2),
-                ("Pittsburgh","Raleigh",2),("Pittsburgh","Toronto",2),
-                ("Pittsburgh","New York",2),("Toronto","Montreal",3),
-                ("Montreal","Boston",2),("Montreal","New York",3),
-                ("Boston","New York",2),("New York","Boston",2),
-                ("Boston","Washington",2),("Washington","Raleigh",2,)]
-
-    liaisons.sort(key=lambda x: x[2]) # on trie les liaisons par longueur
+    liaisons.sort(key=lambda x: x[2])  # on trie les liaisons par longueur
     nombre_villes = 36
 
     print(Kruskal(liaisons, nombre_villes))
